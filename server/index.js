b1e3bef4475f35901366ddaee2e1c97f24110967
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-09 23:20:42 +0100

    set up port

--- server/index.js
+++ server/index.js
@@ -3,3 +3,8 @@ const app = express();
 const cors = require("cors");
 // by usiing pool lib i can run queries with postgres
 const pool = require("./db");
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`serve is running in port: ${port}`);
+})

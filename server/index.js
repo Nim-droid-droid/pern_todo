5762fa3ddd33f348c5ca64559ff27e9cbc149d5a
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-10 22:31:27 +0100

    set up middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const pool = require("./db");
 const port = 5000;
 
+// Express MIDDLEWARE
+app.use(cors() )
+app.use(express.json() )
+
 app.listen(port, () => {
   console.log(`serve is running in port: ${port}`);
 })

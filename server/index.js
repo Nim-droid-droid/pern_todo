a1189dc17689a455691e56deaa04e0c8eb29d470
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-11 23:03:39 +0100

    file modified

--- server/index.js
+++ server/index.js
@@ -5,8 +5,13 @@ const cors = require("cors");
 const pool = require("./db");
 const port = 5000;
 
-// Express MIDDLEWARE
+// Express MIDDLEWARE //
+// everytime u use middlewear u have to use use()
 app.use(cors() )
+// whenever im building fullstack app i need to get data from the client side, only way to get that data is from the request.body property thats inside the request obj that comes with the request sent from the clients machine to the server
+/*This middleware is used to parse incoming requests with JSON payloads.
+When your Express application receives an HTTP request with a JSON payload (typically sent with a Content-Type: application/json header), the express.json() middleware parses the JSON data and makes it available in the req.body property of the request object. This allows you to access the JSON data sent by the client in your route handlers. */
+// give me acces to req.body so i can get the JSON data
 app.use(express.json() )
 
 app.listen(port, () => {

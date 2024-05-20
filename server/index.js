658985d971b3b56f69ef34d346476e1a10e2162c
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-20 23:42:16 +0100

    update error handling for todos post route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 // by usiing pool lib i can run queries with postgres
 const pool = require("./db");
 const port = 5000;
+// whenever im building fullstack app i need to get data from the client side, only way to get that data is from the request.body property thats inside the request obj that comes with the request sent from the clients machine to the server
+/*This middleware is used to parse incoming requests with JSON payloads.
+When your Express application receives an HTTP request with a JSON payload (typically sent with a Content-Type: application/json header), the express.json() middleware parses the JSON data and makes it available in the req.body property of the request object. This allows you to access the JSON data sent by the client in your route handlers. */
+// give me acces to req.body so i can get the JSON data
+app.use(express.json() )  //req.body
 
 // ROUTES //
 // Im going to use the pool connection to run queries on each of these routes:
@@ -15,9 +20,12 @@ const port = 5000;
   // Async cuz whenever we create data its going to take some time to get data back. Async provides me with await which waits for the func to complete before it continues.
   // Express has no opinion on how you fetch data from the server, so it uses the most basic approach which is to use the API built into the browser to make asynchronous request to different network resources by using HTTP request
 // app.post(endpoint, callback)
-app.post("/todos", async(req, res)=>{
+app.post("/todos", async(req, res)=>{  
+  // need to do error handling if its 1 of the 2 things: data from other sources or user input
+  // user input error handling
   try {
-    
+    // Get data from clients to determine what exactly I'm going to add
+    console.log(req.body)
   } catch (err) {
     console.error(err.message);
   }
@@ -33,11 +41,6 @@ app.post("/todos", async(req, res)=>{
 // Express MIDDLEWARE //
 // everytime u use middlewear u have to use use()
 app.use(cors() )
-// whenever im building fullstack app i need to get data from the client side, only way to get that data is from the request.body property thats inside the request obj that comes with the request sent from the clients machine to the server
-/*This middleware is used to parse incoming requests with JSON payloads.
-When your Express application receives an HTTP request with a JSON payload (typically sent with a Content-Type: application/json header), the express.json() middleware parses the JSON data and makes it available in the req.body property of the request object. This allows you to access the JSON data sent by the client in your route handlers. */
-// give me acces to req.body so i can get the JSON data
-app.use(express.json() )
 
 app.listen(port, () => {
   console.log(`serve is running in port: ${port}`);

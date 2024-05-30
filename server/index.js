9bb09093e4d5ff48a46424a52647d2a6011679fc
Nim-droid-droid <levkoz2222@outlook.com>
2024-05-30 22:32:46 +0100

    create route to get to dos based on id

--- server/index.js
+++ server/index.js
@@ -4,11 +4,16 @@ const cors = require("cors");
 // by using pool lib (pg library (node-postgres)) i can run queries on postgres DB
 const pool = require("./db");
 const port = 5000;
+
+// Express MIDDLEWARE //
+// everytime u use middlewear u have to use use()
+app.use(cors() );
+
 // whenever im building fullstack app i need to get data from the client side, only way to get that data is from the request.body property thats inside the request obj that comes with the request sent from the clients machine to the server
 /*This middleware is used to parse incoming requests with JSON payloads.
 When your Express application receives an HTTP request with a JSON payload (typically sent with a Content-Type: application/json header), the express.json() middleware parses the JSON data and makes it available in the req.body property of the request object. This allows you to access the JSON data sent by the client in your route handlers. */
 // give me acces to req.body so i can get the JSON data
-app.use(express.json() )  //req.body
+app.use(express.json() );  //req.body
 
 // ROUTES //
 // RESTful API in Postgres:
@@ -105,14 +110,26 @@ app.get("/todos", async(req, res)=>{
  }
 )
 
+// GET/READ    
 // Get a specific todo based on id
+app.get("/todos/:id", async(req, res)=>{
+  try {
+    console.log(req.params)
+
+    const {id} = req.params;
+
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+
+    res.json(todo.rows[0])
+  } catch (err) {
+    // Logs the error message
+    console.error(err.message)
+  }
+ }
+)
 // PUT/UPDATE    Update a to do 
 // DELETE/DELETE    Delete a to do 
 
-
-// Express MIDDLEWARE //
-// everytime u use middlewear u have to use use()
-app.use(cors() )
 
 app.listen(port, () => {
   console.log(`serve is running in port: ${port}`);

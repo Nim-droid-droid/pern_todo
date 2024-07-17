c9a3465fcc6179cea8b97fd15f83ec8be97003d8
Nim-droid-droid <levkoz2222@outlook.com>
2024-07-17 23:25:25 +0100

    set up state for todos

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 export default function ListTodos() {
+  // Initially this arr is empty but i will put all the fetched data/todos in here. 
+  const [todos, setTodos] = useState([]);
 
   return (
     <>

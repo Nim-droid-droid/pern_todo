99f289b03e86db229d006470dc35119ac34b7ea0
Nim-droid-droid <levkoz2222@outlook.com>
2024-07-18 22:02:48 +0100

    set up async func for get todos

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,15 @@ export default function ListTodos() {
   // Initially this arr is empty but i will put all the fetched data/todos in here. 
   const [todos, setTodos] = useState([]);
 
+  // Make fetch request to my RESTful API Each time this component renders
+  const getTodos = async () => {
+    try {
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+
   return (
     <>
 

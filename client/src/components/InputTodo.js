8baf7434ec9d362dfc46dc98b05d818ada836f3b
Nim-droid-droid <levkoz2222@outlook.com>
2024-08-19 00:16:41 +0100

    file modified

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -8,8 +8,8 @@ export default function InputTodo() {
       <div></div>
       <h1 className="text-center mt-5">PERN Todo List</h1>
 
+      {/* connect form to onSubmitForm fun. Each time form submits onSubmitForm fires */}
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-
         <input
           type="text"
           className="form-control"

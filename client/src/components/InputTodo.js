5a2c61a7968d9f3e975a2184a6156a3184bb0352
Nim-droid-droid <levkoz2222@outlook.com>
2024-08-20 20:24:43 +0100

    file modified

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -10,6 +10,7 @@ export default function InputTodo() {
 
       {/* connect form to onSubmitForm fun. Each time form submits onSubmitForm fires */}
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+      {/* value={description}    React will force the input to always have the value you passed, in this case description var */}
         <input
           type="text"
           className="form-control"

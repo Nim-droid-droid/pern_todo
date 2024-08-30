aa2e781ab6cb16676c0a5175f4c5b0074680f688
Nim-droid-droid <levkoz2222@outlook.com>
2024-08-31 00:55:49 +0100

    store res from call

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -8,7 +8,8 @@ export default function InputTodo() {
     e.preventDefault();
     try {
       const body = { description };
-
+      const response = await fetch({
+      });
     } catch (err) {
       console.error(err.message);
     }

e5e9dc974e151baf46d24a00bf6c8a9f4e231406
Nim-droid-droid <levkoz2222@outlook.com>
2024-09-22 20:26:29 +0100

    file modified

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE perntodo;
 
 CREATE TABLE todo(
+  -- this column is going to make each to do unique so i can do CRUD on it e.g. update/delete/create a todo
   todo_id SERIAL PRIMARY KEY,
   -- description Can store max characters of 255
   description VARCHAR(255)

5f740ba516f26abbe056ee47cf23fbb0a142253e
Nim-droid-droid <levkoz2222@outlook.com>
2024-09-24 21:34:50 +0100

    file modified

--- server/database.sql
+++ server/database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE perntodo;
 
 CREATE TABLE todo(
   -- this column is going to make each to do unique so i can do CRUD on it e.g. update/delete/create a todo
-  -- SERIAL increments my primary key val for me to insure uniqueness 
+  -- SERIAL increments my primary key val for me to insure uniqueness - SERIAL data type stores a sequential integer, of the INT data type.
   todo_id SERIAL PRIMARY KEY,
   -- description Can store max characters of 255
   description VARCHAR(255)

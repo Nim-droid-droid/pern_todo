e79633d4c421ccf84c3097bed30f0e27dd746097
Nim-droid-droid <levkoz2222@outlook.com>
2025-01-08 23:04:13 +0000

    file modified

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 
 // Functionality not tested
 function EditTodo(props) {
+  const [value, setValue] = useState(props.task);
+
   return (
     <>
       <button>Edit</button>

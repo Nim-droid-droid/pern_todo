1c23ba8e8b505d625b7aa2834e4344b2001654bf
Nim-droid-droid <levkoz2222@outlook.com>
2025-01-10 07:49:13 +0000

    file modified

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 
-// Functionality not tested - fix props names
+// Functionality not tested - fix props names & make them match
 function EditTodo(props) {
   const [value, setValue] = useState(props.task);
 

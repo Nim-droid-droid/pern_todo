db3696b14d81746078e2297573b7e750772b98a0
Nim-droid-droid <levkoz2222@outlook.com>
2025-02-03 23:38:50 +0000

    file modified

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -16,7 +16,7 @@ function EditTodo(props) {
   return (
     <>
       <form onSubmit={handleSubmit} className="TodoForm">
-        <input type="text" value={value}/>
+        <input type="text" value={value} onChange={} />
       </form>
     </>
   )

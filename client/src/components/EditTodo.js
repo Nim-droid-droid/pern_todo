60c6e120b794e3903697644eadca71db9c5e534b
Nim-droid-droid <levkoz2222@outlook.com>
2025-02-23 00:47:11 +0000

    file modified

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -17,7 +17,7 @@ function EditTodo(props) {
     <>
       <form onSubmit={handleSubmit} className="TodoForm">
         <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task'/>
-        <button type="submit" className='todo'>Add Task</button>
+        <button type="submit" className='todo-btn'>Add Task</button>
       </form>
     </>
   )

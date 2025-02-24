f85abe91df8da7e67b9ec7e0bbb854bbd470c725
Nim-droid-droid <levkoz2222@outlook.com>
2025-02-24 22:05:06 +0000

    file modified

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 
 // Functionality not tested - fix props names & make them match
-function EditTodo(props) {
+export function EditTodo(props) {
   const [value, setValue] = useState(props.task);
 
   function handleSubmit(e){
@@ -22,4 +22,3 @@ function EditTodo(props) {
     </>
   )
 }
-export default EditTodo;

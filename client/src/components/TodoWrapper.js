9191ae7d10d9abec7f98d21733960f1a5b1d5b87
Nim-droid-droid <levkoz2222@outlook.com>
2025-03-27 22:58:55 +0000

    file modified

--- client/src/components/TodoWrapper.js
+++ client/src/components/TodoWrapper.js
@@ -17,7 +17,7 @@ export default function TodoWrapper(){
       }
     ]);
   }
-    const deleteTodo = (id) => setTodos(todos());
+    const deleteTodo = (id) => setTodos(todos.filter());
     
   return();
 }

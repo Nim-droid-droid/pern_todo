c0e01797c469aefda887ed682570a65afbbeefb1
Nim-droid-droid <levkoz2222@outlook.com>
2025-06-15 22:16:47 +0100

    file modified

--- client/src/components/TodoWrapper.js
+++ client/src/components/TodoWrapper.js
@@ -57,6 +57,7 @@ export default function TodoWrapper(){
             task={todo}
             deleteTodo={deleteTodo}
             editTodo={editTodo}
+            toggleComplete={}
           />
         )
       )}

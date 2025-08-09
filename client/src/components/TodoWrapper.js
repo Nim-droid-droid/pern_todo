6876807770c34175e52edab3dd8333663a2a6d17
Nim-droid-droid <levkoz2222@outlook.com>
2025-08-09 18:42:43 +0100

    file modified

--- client/src/components/TodoWrapper.js
+++ client/src/components/TodoWrapper.js
@@ -12,6 +12,7 @@ import { v4 as uuidv4 } from "uuid"; // Importing UUID library for generating un
  * Layout also similar to Structure app, clean lines and a very simple format.
  * ADDITIONAL STYLING
     Inspiration
+    Colour palette
 */
 
 export default function TodoWrapper(){
